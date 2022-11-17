1d64248d3aee5efcebd52d3cdaccbb140a62f375
Daniyal Ganiuly <daniyal.ganiuly@gmail.com>
2022-11-17 17:48:22 +0600

    Get all games in list, no styles applied

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from "../../service/http.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {APIResponse, Game} from "../../model/models";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public sort!: string
+  public games!: Array<Game>
+  private routeSub!: Subscription
+  private gameSub!: Subscription
+
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search'])
+      } else {
+        this.searchGames('metacrit')
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.gameSub = this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results
+        console.log(gameList)
+      })
   }
 
 }
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {APIResponse, Game} from "../model/models";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  BASE_URL = 'https://rawg-video-games-database.p.rapidapi.com'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+  let params = new HttpParams().set('ordering', ordering)
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search)
+    }
+
+    return this.http.get<APIResponse<Game>>(this.BASE_URL+`/games`, {
+      params: params
+    })
+  }
 }
